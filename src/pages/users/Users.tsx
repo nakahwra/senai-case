004d130fbb6c5898f51ffbb6c12c422027a2a634
Lucas Nakahara <nakahwra@gmail.com>
2023-02-18 17:32:02 -0300

    🔥  remove commented code

--- src/pages/users/Users.tsx
+++ src/pages/users/Users.tsx
@@ -43,25 +43,6 @@ function Users() {
     if (shouldDelete) await deleteUser.mutateAsync(id);
   };
 
-  // <div className="relative overflow-x-auto">
-  //   <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
-  //     <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
-  //       <tr>
-  //         <th scope="col" className="px-4 py-3">
-  //           ID
-  //         </th>
-  //         <th scope="col" className="px-4 py-3">
-  //           Username
-  //         </th>
-  //         <th scope="col" className="px-4 py-3">
-  //           Email
-  //         </th>
-  //         <th scope="col" className="px-4 py-3" />
-  //       </tr>
-  //     </thead>
-  //   </table>
-  // </div>;
-
   return (
     <PageContainer
       title="Usuários"
