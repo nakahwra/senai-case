c91b70064fb21627f1807d3d46467a20f602456e
Lucas Nakahara <nakahwra@gmail.com>
2023-02-18 17:14:15 -0300

    🔧 add environments mocks to mirage

--- src/services/mirage/index.ts
+++ src/services/mirage/index.ts
@@ -1,10 +1,16 @@
-import { ActiveModelSerializer, createServer, Model } from "miragejs";
+import { ActiveModelSerializer, createServer, Factory, Model } from "miragejs";
 
 type User = {
   id: number;
   username: string;
   email: string;
   password: string;
+};
+
+type Environment = {
+  name: string;
+  macAddresses: string[];
+  classes: string[];
 };
 
 export function makeServer() {
@@ -15,15 +21,41 @@ export function makeServer() {
 
     models: {
       user: Model.extend<Partial<User>>({}),
+      environment: Model.extend<Partial<Environment>>({}),
+    },
+
+    factories: {
+      user: Factory.extend({
+        id(i: number) {
+          return i + 1000;
+        },
+        username(i: number) {
+          return `João Pedro ${i + 1}`;
+        },
+        email(i: number) {
+          return `jp${i + 1}@email.com`;
+        },
+        password() {
+          return "123456";
+        },
+      }),
+
+      environment: Factory.extend({
+        name(i: number) {
+          return `Environment ${i + 1}`;
+        },
+        macAddresses() {
+          return ["00:00:00:00:00:00"];
+        },
+        classes() {
+          return ["Capacete", "Óculos", "Protetor auricular"];
+        },
+      }),
     },
 
     seeds(server) {
-      server.create("user", {
-        id: 1234,
-        username: "John Doe",
-        email: "johndoe@example.com",
-        password: "password123",
-      });
+      server.createList("user", 5);
+      server.createList("environment", 5);
     },
 
     routes() {
